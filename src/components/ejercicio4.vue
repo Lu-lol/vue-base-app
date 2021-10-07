c173fe7924bd6fb0a7c9d35021827337a7df8dc8
oal <toviya3135@gmail.com>
2021-10-07 17:03:29 -0300

    4 donnnnnnne

---
+++ src/components/ejercicio4.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    data() {
+        return {
+            list: []
+        }
+    },
+    methods: {
+        add: function() {
+            this.list.push(this.text);
+        },
+    },
+}
+</script>
+<template>
+    <div>
+        <ul>
+            <input type="text" v-model="text">
+            <button @click="add()">Agregar</button>
+            <li v-for="text in list" v-bind:key="text.id">{{text}}</li>
+        </ul>
+    </div>
+</template>
